63193280255c1f1b093bd3057d86ef824b0ece40
amirianos <amisoltani@protonmail.com>
2022-04-26 10:34:40 +0430

    add config generator

--- installer.sh
+++ installer.sh
@@ -1,6 +1,8 @@
 #! /bin/sh 
 
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install -y ansible
+ansible-config init --disabled > ~/ansible.cfg
+sudo mv ~/ansible.cfg /etc/ansible/ansible.cfg
