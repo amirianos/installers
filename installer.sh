d5fa93510732e275b24c47a0a39caf7e55ca25bd
amirianos <amisoltani@protonmail.com>
2022-06-19 14:04:14 +0430

    fix host checking part

--- installer.sh
+++ installer.sh
@@ -5,5 +5,5 @@ sudo apt install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 ansible-config init --disabled > ~/ansible.cfg
-echo host_key_checking=False >> ~/ansible.cfg
+sed -i 's/;host_key_checking=True/host_key_checking=False/' ~/ansible.cfg
 sudo mv ~/ansible.cfg /etc/ansible/ansible.cfg
